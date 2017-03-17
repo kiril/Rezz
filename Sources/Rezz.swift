2b350c0e68647d09782d35a08f3f34702cfc26d0
Kiril Savino <kiril.savino@gmail.com>
2017-03-17 13:37:58 -0400

    hm

---
+++ Sources/Rezz.swift
@@ -0,0 +1,20 @@
+import Foundation
+import CRezz
+
+func resolve(hostname: String) -> String? {
+    // get our hostname as a pointer we can pass
+    let hostnameData = hostname.data(using: String.Encoding.utf8)!
+    let charPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: hostname.characters.count)
+    hostnameData.copyBytes(to: charPointer, count: hostname.characters.count)
+    let hostnamePointer = UnsafeMutableRawPointer(charPointer).bindMemory(to: Int8.self, capacity: hostname.characters.count)
+
+    // this is the container we'll get the return in
+    let ipPointer = UnsafeMutablePointer<Int8>.allocate(capacity: 100)
+
+    // call the C function!
+    hostname_to_ip(hostnamePointer, ipPointer)
+
+    // unpack the IP!
+    let stringPointer = UnsafeMutableRawPointer(ipPointer).bindMemory(to: UInt8.self, capacity: 100)
+    return String(cString: stringPointer)
+}
